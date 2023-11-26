59128f2ad81b1c8a13bae78532803b6a11ac60c6
Sreeja Malladi <sreejamalladi11@gmail.com>
2023-11-25 22:37:14 -0800

    Initial Commit

---
+++ batchscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+# 
+#SBATCH --job-name=hecktor 
+#SBATCH --output=/WAVE/users/unix/smalladi/varian_ml/logs/outputlogs-%j.out 
+# 
+#SBATCH --partition=gpu
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=16G
+#SBATCH --time=2-00:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=smalladi@scu.edu
+#SBATCH --mail-type=FAIL,END
+#
+module load Anaconda3
+conda activate venv
+cd /WAVE/users/unix/smalladi/varian_ml/Sreeja_temp
+python train.py
